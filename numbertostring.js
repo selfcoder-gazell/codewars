9d57094f197beb0edb84c890906e2555ee9034a4
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2024-01-15 17:19:45 +0000

    number to string solution

---
+++ numbertostring.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+// Convert a Number to a String - codewars
+
+function numberToString(num) {
+    return num.toString();
+  }
+console.log(numberToString(18));
+
+console.log(typeof numberToString(18));
+
+
